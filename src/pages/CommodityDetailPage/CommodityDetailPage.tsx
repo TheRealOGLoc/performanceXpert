c1b0c114a55f21c9f9337593eabc940d58b3a15b
Yuchen Liu <lyc990312@hotmail.com>
2024-04-19 23:48:58 +1000

    Add the detail page

--- src/pages/CommodityDetailPage/CommodityDetailPage.tsx
+++ src/pages/CommodityDetailPage/CommodityDetailPage.tsx
@@ -1,13 +1,53 @@
 import React from "react"
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { findCommodity } from "../../utilities/data-service"
 
 export default function CommodityDetailPage() {
-    const {id} = useParams()
+    const { id } = useParams()
+    const [detail, setDetail] = useState({})
+    const [count, setCount] = useState(1)
+    const [stock, setStock] = useState(0)
 
+    useEffect(() => {
+        async function getDetail() {
+            const commodityDetail = await findCommodity({ id: id })
+            setDetail(commodityDetail)
+            setStock(commodityDetail.stock)
+        }
+        getDetail()
+    }, [id])
 
+    const handleMinus = () => {
+        if (count > 1) {
+            setCount(count - 1)
+        }
+    }
+
+    const handleAdd = () => {
+        if (count != stock) {
+            setCount(count + 1)
+        }
+    }
 
     return (
-        <div></div>
+        <div>
+            <img src={detail.url} alt={detail.url} />
+            <div>{detail.name}</div>
+            <div>Price: ${detail.price}</div>
+            <div>SKU: {detail.SKU}</div>
+            <div>
+                <div>Quantity: </div>
+                <div>
+                    <button onClick={handleMinus} >-</button>
+                    <div>{count}</div>
+                    <button onClick={handleAdd} >+</button>
+                </div>
+            </div>
+            <button>Add to cart</button>
+
+            <div>Description: </div>
+            <div>{detail.description}</div>
+        </div>
     )
 }
